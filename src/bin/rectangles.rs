0f9b18d5a1547fb7262313d0aa4b3e9051a77f5a
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-06-15 22:24:07 +0700

    использование макроса dbg

--- src/bin/rectangles.rs
+++ src/bin/rectangles.rs
@@ -1,8 +1,19 @@
+#[derive(Debug)]
+struct Rectangle {
+  width: u32,
+  height: u32
+}
 fn main() {
-  let rect1 = (30, 50);
-  println!("The area of the rectangle is {} square pixels", area(rect1))
+  let scale = 2;
+  let rect1 = Rectangle {
+    width: dbg!(30 * scale),
+    height: 50
+  };
+  dbg!(&rect1);
+  println!("rect is {:#?}", rect1);
+  println!("The area of the rectangle is {} square pixels", area(&rect1));
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-  dimensions.0 * dimensions.1
+fn area(rectangle: &Rectangle) -> u32 {
+  rectangle.width * rectangle.height
 }
