35032a003f2a95bc81e5a724a304be3f7102aeca
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-06-15 22:42:21 +0700

    feat(rectangles): методы

--- src/bin/rectangles.rs
+++ src/bin/rectangles.rs
@@ -3,17 +3,19 @@ struct Rectangle {
   width: u32,
   height: u32
 }
+
+impl Rectangle {
+  fn area(&self) -> u32 {
+    self.width * self.height
+  }
+}
 fn main() {
   let scale = 2;
   let rect1 = Rectangle {
-    width: dbg!(30 * scale),
+    width: 30 * scale,
     height: 50
   };
-  dbg!(&rect1);
-  println!("rect is {:#?}", rect1);
-  println!("The area of the rectangle is {} square pixels", area(&rect1));
+
+  println!("The area of the rectangle is {} square pixels", rect1.area());
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-  rectangle.width * rectangle.height
-}
