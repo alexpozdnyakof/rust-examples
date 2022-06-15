755c04e80e070d353e8cb06bf5fd6ef7047f87ca
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-06-15 23:52:10 +0700

    feat: добавлены связанные функции rectangles

--- src/bin/rectangles.rs
+++ src/bin/rectangles.rs
@@ -14,10 +14,12 @@ fn main() {
     width: 30,
     height: 50,
   };
+
   let rect2 = Rectangle {
       width: 10,
       height: 40,
   };
+
   let rect3 = Rectangle {
       width: 60,
       height: 45,
