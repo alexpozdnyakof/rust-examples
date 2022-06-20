301db7a6eca9b49266781b9cf3611a60fce31a99
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-06-20 23:12:27 +0700

    feat: перечисления enums

--- src/bin/enums.rs
+++ src/bin/enums.rs
@@ -1,24 +1,56 @@
 fn main () {
-    enum IpAddrKind {
-        V4,
-        V6,
+    #[derive(Debug)]
+    enum UsState {
+        Alabama,
+        Alaska
     }
 
-    struct IpAddr {
-        kind: IpAddrKind,
-        address: String,
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter(UsState)
+    }
+
+    fn value_in_cents(coin: Coin) -> u8 {
+        match coin {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter(state) => {
+                println!("State quarter from {:?}!", state);
+                25
+            }
+        }
+    }
+
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => move_player(other),
+        // _ => ()
     }
 
 
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
+    let mut count = 0;
 
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
 
 }
 
